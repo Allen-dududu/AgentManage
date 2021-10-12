61b333efbbfa96aa325938253dd7274d5808742d
allendududu <18133679546@163.com>
2021-10-12 23:39:10 +0800

    role const

---
+++ AgentManage/Controllers/Role.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AgentManage.Controllers
+{
+    public class Role
+    {
+        public static string Administrator = "administrator";
+
+        public static string Manager = "manager";
+
+        public static string Agent = "agent";
+    }
+}
